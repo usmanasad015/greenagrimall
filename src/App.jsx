12194b327ac7a600d84a6f64a7f1d4a66990b617
usmanasad015 <usman.asad015@gmail.com>
2025-12-10 13:24:11 +0500

    Fix Gallery import case

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import Home from "../components/home/Home";
 import Gallery from "../components/Gallery";
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  useLocation,
-} from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
 import Header from "../components/header/Header";
 
 function AnimatedRoutes() {
-  const backgroundStyle = {
-    backgroundImage: "url('/agrimall_onlylogo.png')",
-    backgroundRepeat: "no-repeat",
-    backgroundPosition: "center",
-    backgroundSize: "contain",
-    minHeight: "100vh",
-  };
   const location = useLocation();
   const isGalleryPage = location.pathname === "/gallery";
 
@@ -34,10 +22,10 @@ function AnimatedRoutes() {
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
       <AnimatedRoutes />
-    </Router>
+    </>
   );
 }
 
